8a88ed4d33b833033ad32e2afed97de6781c794f
Max Podloobniy <max.podloobniy@gmail.com>
2026-03-04 00:12:32 +0000

    feat: add ssh auth infrastructure

--- ssh-auth/ec2.tf
+++ ssh-auth/ec2.tf
@@ -22,7 +22,12 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["al2023-ami-2023*-x86_64"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
   }
 }
 
@@ -30,11 +35,12 @@ data "aws_ami" "amazon_linux" {
 # EC2 Instance
 # ──────────────────────────────────────────────
 resource "aws_instance" "this" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.this.key_name
-  vpc_security_group_ids = [data.aws_security_group.main.id]
-  subnet_id              = data.aws_subnets.public.ids[0]
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.this.key_name
+  vpc_security_group_ids      = [data.aws_security_group.main.id]
+  subnet_id                   = data.aws_subnets.public.ids[0]
+  associate_public_ip_address = true
 
   tags = {
     Name    = "cmtr-bgxc7tqb-ec2"
