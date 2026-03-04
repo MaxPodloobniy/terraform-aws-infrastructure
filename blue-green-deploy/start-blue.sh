cccba474533ae42602ccb3b4a32ebb5bfb055bcf
Max Podloobniy <max.podloobniy@gmail.com>
2026-03-04 16:43:26 +0000

    fix: add proper green deployment script

--- blue-green-deploy/start-blue.sh
+++ blue-green-deploy/start-blue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dnf update -y
-dnf install -y httpd
+yum update -y
+yum install -y httpd
 systemctl enable httpd
 systemctl start httpd
 echo "<h1>Blue Environment</h1>" > /var/www/html/index.html
